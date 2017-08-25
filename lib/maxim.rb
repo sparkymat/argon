602580219feb1b652c7fa589d7ca5459534b6fa9
Ajith Hussain <csy0013@googlemail.com>
2017-08-25 19:30:01 +1000

    Added stub and specs for state_machine()

--- lib/maxim.rb
+++ lib/maxim.rb
@@ -9,6 +9,50 @@ module Maxim
   extend ActiveSupport::Concern
 
   module ClassMethods
+    def state_machine(mapping)
+      raise Maxim::Error.new("status_machine() has to be called on a Hash") unless mapping.is_a?(Hash)
+      raise Maxim::Error.new("status_machine() has to specify a field and the mappings") unless mapping.keys.count == 1 && mapping.keys.first.is_a?(Symbol) && mapping.values.first.is_a?(Hash)
+      raise Maxim::Error.new("status_machine() should have (only) the following mappings: states, events, edges, on_successful_transition, on_failed_transition") if mapping.values.first.keys.sort != %i(states events edges on_successful_transition on_failed_transition).sort
+      
+      field                    = mapping.keys.first
+      states_map               = mapping.values.first[:states]
+      events_list              = mapping.values.first[:events]
+      edges_list               = mapping.values.first[:edges]
+      on_successful_transition = mapping.values.first[:on_successful_transition]
+      on_failed_transition     = mapping.values.first[:on_failed_transition]
+      
+      raise Maxim::Error.new("`states` should be a Hash") unless states_map.is_a?(Hash)
+      raise Maxim::Error.new("`states` does not specify any states") if states_map.empty?
+      raise Maxim::Error.new("`states` must be a mapping of Symbols to unique Integers") unless states_map.keys.map(&:class).uniq == [Symbol] && states_map.values.map(&:class).uniq == [Integer] && states_map.values.uniq.sort == states_map.values.sort
+      states_map.keys.each do |state_name|
+        raise Maxim::Error.new("`#{state_name}` is an invalid state name. `#{self.name}.#{state_name}` method already exists") if self.singleton_methods.include?(state_name)
+        raise Maxim::Error.new("`#{state_name}` is an invalid state name. `#{self.name}##{state_name}?` method already exists") if self.instance_methods.include?("#{state_name}?".to_sym)
+      end
+
+      raise Maxim::Error.new("`events` should be an Array of Symbols") if !events_list.is_a?(Array) || events_list.map(&:class).uniq != [Symbol]
+      events_list.each do |event_name|
+        raise Maxim::Error.new("`#{event_name}` is not a valid event name. `#{self.name}##{event_name}` method already exists") if self.instance_methods.include?(event_name)
+      end
+
+      raise Maxim::Error.new("`edges` should be an Array of Hashes, with keys: from, to, action, post_lock_callback (optional), in_lock_callback (optional), on_event (optional)") if !edges_list.is_a?(Array) || edges_list.map(&:class).uniq != [Hash]
+      edges_list.each_with_index do |edge_details, index|
+        raise Maxim::Error.new("`edges` should be an Array of Hashes, with keys: from, to, action, post_lock_callback (optional), in_lock_callback (optional), on_event (optional)") unless edge_details.keys.to_set.subset?([:from, :to, :action, :post_lock_callback, :in_lock_callback, :on_event].to_set) && [:from, :to, :action].to_set.subset?(edge_details.keys.to_set)
+        raise Maxim::Error.new("`edges[#{index}].from` is not a valid state") unless states_map.keys.include?(edge_details[:from])
+        raise Maxim::Error.new("`edges[#{index}].to` is not a valid state") unless states_map.keys.include?(edge_details[:to])
+        raise Maxim::Error.new("`edges[#{index}].action` is not a Symbol") unless edge_details[:action].is_a?(Symbol)
+        raise Maxim::Error.new("`#{edge_details[:action]}` is an invalid action name. `#{self.name}##{edge_details[:action]}` method already exists") if self.instance_methods.include?(edge_details[:action])
+        raise Maxim::Error.new("`edges[#{index}].in_lock_callback` is not a Symbol") if !edge_details[:in_lock_callback].nil? && !edge_details[:in_lock_callback].is_a?(Symbol)
+        raise Maxim::Error.new("`edges[#{index}].post_lock_callback` is not a Symbol") if !edge_details[:post_lock_callback].nil? && !edge_details[:post_lock_callback].is_a?(Symbol)
+        raise Maxim::Error.new("`#{edge_details[:on_event]}` (`edges[#{index}].on_event`) is not a valid event") if !edge_details[:on_event].nil? && !events_list.include?(edge_details[:on_event])
+      end
+
+      raise Maxim::Error.new("`on_successful_transition` must be a lambda of signature `(from:, to:)`") if !on_successful_transition.nil? && !on_successful_transition.is_a?(Proc)
+      raise Maxim::Error.new("`on_successful_transition` must be a lambda of signature `(from:, to:)`") if on_successful_transition.parameters != [[:keyreq, :from],[:keyreq, :to]]
+
+      raise Maxim::Error.new("`on_failed_transition` must be a lambda of signature `(from:, to:)`") if !on_failed_transition.nil? && !on_failed_transition.is_a?(Proc)
+      raise Maxim::Error.new("`on_failed_transition` must be a lambda of signature `(from:, to:)`") if on_failed_transition.parameters != [[:keyreq, :from],[:keyreq, :to]]
+    end
+
     def state_enum(mapping)
       raise Maxim::Error.new("status_enum called on bad params") unless mapping.is_a?(Hash)
       raise Maxim::Error.new("status_enum called on bad params") unless mapping.keys.count == 1 && mapping.keys.first.is_a?(Symbol) && mapping.values.first.is_a?(Hash)
--- spec/maxim_spec.rb
+++ spec/maxim_spec.rb
@@ -33,4 +33,522 @@ RSpec.describe Maxim do
 
     expect(SampleClass).to respond_to(:states)
   end
+
+  context 'checks params structure' do
+    after do
+      Object.send(:remove_const, :SampleClass)
+    end
+
+    it 'should not allow without args' do
+      expect {
+        class SampleClass
+          include Maxim
+          state_machine
+        end
+      }.to raise_error(ArgumentError)
+    end
+
+    it 'should not allow on non-Hash' do
+      expect {
+        class SampleClass
+          include Maxim
+          state_machine :state
+        end
+      }.to raise_error(Maxim::Error, "status_machine() has to be called on a Hash")
+    end
+
+    it 'should only allow a field and mappings' do
+      expect {
+        class SampleClass
+          include Maxim
+          state_machine state: :foo
+        end
+      }.to raise_error(Maxim::Error, "status_machine() has to specify a field and the mappings")
+    end
+
+    it 'should only allow states, events, edges and transition callbacks' do
+      expect {
+        class SampleClass
+          include Maxim
+          state_machine state: {
+            foo: 1,
+            bar: 2,
+          }
+        end
+      }.to raise_error(Maxim::Error, "status_machine() should have (only) the following mappings: states, events, edges, on_successful_transition, on_failed_transition")
+    end
+
+    it 'should only allow Hash for states' do
+      expect {
+        class SampleClass
+          include Maxim
+          state_machine state: {
+            states: {
+              foo: 1,
+              bar: 2,
+            },
+            bar: 2,
+          }
+        end
+      }.to raise_error(Maxim::Error, "status_machine() should have (only) the following mappings: states, events, edges, on_successful_transition, on_failed_transition")
+    end
+
+    it 'should not allow empty states definitions' do
+      expect {
+        class SampleClass
+          include Maxim
+          state_machine state: {
+            states:                   {},
+            events:                   2,
+            edges:                    3,
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`states` does not specify any states")
+    end
+
+    it 'should only allow state mappings to non Integers' do
+      expect {
+        class SampleClass
+          include Maxim
+          state_machine state: {
+            states: {
+              foo: 'hello',
+              bar: 4.7,
+            },
+            events:                   2,
+            edges:                    3,
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`states` must be a mapping of Symbols to unique Integers")
+    end
+
+    it 'should not allow state mappings from non-Symbols' do
+      expect {
+        class SampleClass
+          include Maxim
+          state_machine state: {
+            states: {
+              1   => 1,
+              nil => 2,
+            },
+            events:                   2,
+            edges:                    3,
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`states` must be a mapping of Symbols to unique Integers")
+    end
+
+    it 'should only allow unique Integer mappings for states' do
+      expect {
+        class SampleClass
+          include Maxim
+          state_machine state: {
+            states: {
+              foo: 1,
+              bar: 1,
+            },
+            events:                   2,
+            edges:                    3,
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`states` must be a mapping of Symbols to unique Integers")
+    end
+
+    it 'should prevent clashes between existing singelton methods and state scope methods' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          def self.foo
+          end
+
+          state_machine state: {
+            states: {
+              foo: 1,
+              bar: 2,
+            },
+            events:                   2,
+            edges:                    3,
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`foo` is an invalid state name. `SampleClass.foo` method already exists")
+    end
+
+    it 'should prevent clashes between existing instance methods and state check methods' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          def foo?
+          end
+
+          state_machine state: {
+            states: {
+              foo: 1,
+              bar: 2,
+            },
+            events:                   2,
+            edges:                    3,
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`foo` is an invalid state name. `SampleClass#foo?` method already exists")
+    end
+
+    it 'should only allow events as an array of Symbols' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          state_machine state: {
+            states: {
+              foo: 1,
+              bar: 2,
+            },
+            events:                   2,
+            edges:                    3,
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`events` should be an Array of Symbols")
+    end
+
+    it 'should prevent clashes between existing instance methods and event methods' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          def foo
+          end
+
+          state_machine state: {
+            states: {
+              abc: 1,
+              def: 2,
+            },
+            events: [
+              :foo,
+              :bar
+            ],
+            edges:                    3,
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`foo` is not a valid event name. `SampleClass#foo` method already exists")
+    end
+
+    it 'should only allow edges as an Array of Hashes' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          state_machine state: {
+            states: {
+              abc: 1,
+              def: 2,
+            },
+            events: [
+              :foo,
+            ],
+            edges:                    3,
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`edges` should be an Array of Hashes, with keys: from, to, action, post_lock_callback (optional), in_lock_callback (optional), on_event (optional)")
+    end
+
+    it 'should only allow edges with the right keys' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          state_machine state: {
+            states: {
+              abc: 1,
+              def: 2,
+            },
+            events: [
+              :foo,
+            ],
+            edges: [
+              {from: 1, to: 2},
+              {from: 1, to: 2, action: 3, foo: 4},
+            ],
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`edges` should be an Array of Hashes, with keys: from, to, action, post_lock_callback (optional), in_lock_callback (optional), on_event (optional)")
+    end
+
+    it 'should only allow edges from valid states' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          state_machine state: {
+            states: {
+              abc: 1,
+              def: 2,
+            },
+            events: [
+              :foo,
+            ],
+            edges: [
+              {from: 1, to: 2, action: 3},
+            ],
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`edges[0].from` is not a valid state")
+    end
+
+    it 'should only allow edges to valid states' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          state_machine state: {
+            states: {
+              abc: 1,
+              def: 2,
+            },
+            events: [
+              :foo,
+            ],
+            edges: [
+              {from: :abc, to: 2, action: 3},
+            ],
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`edges[0].to` is not a valid state")
+    end
+
+    it 'should only allow edge action as a valid Symbol' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          state_machine state: {
+            states: {
+              abc: 1,
+              def: 2,
+            },
+            events: [
+              :foo,
+            ],
+            edges: [
+              {from: :abc, to: :def, action: 3},
+            ],
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`edges[0].action` is not a Symbol")
+    end
+
+    it 'should not allow edge action to conflict with an existing method' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          def foo
+          end
+
+          state_machine state: {
+            states: {
+              abc: 1,
+              def: 2,
+            },
+            events: [
+              :ghi,
+            ],
+            edges: [
+              {from: :abc, to: :def, action: :foo},
+            ],
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`foo` is an invalid action name. `SampleClass#foo` method already exists")
+    end
+
+    it 'should only allow edge in_lock_callback as a Symbol' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          state_machine state: {
+            states: {
+              abc: 1,
+              def: 2,
+            },
+            events: [
+              :foo,
+            ],
+            edges: [
+              {from: :abc, to: :def, action: :ghi, in_lock_callback: 22},
+            ],
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`edges[0].in_lock_callback` is not a Symbol")
+    end
+
+    it 'should only allow edge post_lock_callback as a Symbol' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          state_machine state: {
+            states: {
+              abc: 1,
+              def: 2,
+            },
+            events: [
+              :foo,
+            ],
+            edges: [
+              {from: :abc, to: :def, action: :ghi, post_lock_callback: 22},
+            ],
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`edges[0].post_lock_callback` is not a Symbol")
+    end
+
+    it 'should only allow edge on_event from the event list' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          state_machine state: {
+            states: {
+              abc: 1,
+              def: 2,
+            },
+            events: [
+              :foo,
+            ],
+            edges: [
+              {from: :abc, to: :def, action: :ghi, on_event: :bar},
+            ],
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`bar` (`edges[0].on_event`) is not a valid event")
+    end
+
+    it 'should only allow on_successful_transition as a lambda' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          state_machine state: {
+            states: {
+              abc: 1,
+              def: 2,
+            },
+            events: [
+              :foo,
+            ],
+            edges: [
+              {from: :abc, to: :def, action: :ghi},
+            ],
+            on_successful_transition: 4,
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`on_successful_transition` must be a lambda of signature `(from:, to:)`")
+    end
+
+    it 'should only allow on_successful_transition as a lambda(from:, to:)' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          state_machine state: {
+            states: {
+              abc: 1,
+              def: 2,
+            },
+            events: [
+              :foo,
+            ],
+            edges: [
+              {from: :abc, to: :def, action: :ghi},
+            ],
+            on_successful_transition: ->(test:) {},
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`on_successful_transition` must be a lambda of signature `(from:, to:)`")
+    end
+
+    it 'should only allow on_failed_transition as a lambda' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          state_machine state: {
+            states: {
+              abc: 1,
+              def: 2,
+            },
+            events: [
+              :foo,
+            ],
+            edges: [
+              {from: :abc, to: :def, action: :ghi},
+            ],
+            on_successful_transition: ->(from:,to:) {},
+            on_failed_transition:     5,
+          }
+        end
+      }.to raise_error(Maxim::Error, "`on_failed_transition` must be a lambda of signature `(from:, to:)`")
+    end
+
+    it 'should only allow on_failed_transition as a lambda(from:, to:)' do
+      expect {
+        class SampleClass
+          include Maxim
+
+          state_machine state: {
+            states: {
+              abc: 1,
+              def: 2,
+            },
+            events: [
+              :foo,
+            ],
+            edges: [
+              {from: :abc, to: :def, action: :ghi},
+            ],
+            on_successful_transition: ->(from:, to:) {},
+            on_failed_transition:     ->(test:) {},
+          }
+        end
+      }.to raise_error(Maxim::Error, "`on_failed_transition` must be a lambda of signature `(from:, to:)`")
+    end
+  end
 end
